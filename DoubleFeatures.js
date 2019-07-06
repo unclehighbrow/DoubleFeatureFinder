a5cde67557abba29aaf1cfb0fa391d8ded4cd769
Will Carlough <will@carlough.net>
2019-07-06 11:27:28 -0400

    fixing that dup key error

--- DoubleFeatures.js
+++ DoubleFeatures.js
@@ -5,7 +5,7 @@ import {
   Text,
   View,
   TouchableHighlight,
-  ListView,
+  SectionList,
   Linking,
   Dimensions,
 } from 'react-native';
@@ -30,45 +30,48 @@ class DoubleFeatures extends Component {
     this.theatreId = navigation.getParam('theatreId');
     this.dfs = navigation.getParam('dfs');
 
-    var data = {};
-    var sections = [];
+    this.data = []; // [{data: [[theatreId, movieId1, time1, movieId2, time2]], key: theatreId}]
+    var sections = []; // [theatreId]
     this.dfs.map((df) => {
       var section = df[0];
-      if (sections.indexOf(section) === -1) {
+      if (sections.indexOf(section) === -1) { // if there's no section yet, make one
         sections.push(section);
-        data[section] = [];
+        this.data.push({data: [], key: section});
       }
-      data[section].push(df);
+      this.data.forEach((theatre) => { // now find the right section, maybe the one we just put in, and stick it in
+        if (theatre.key === df[0]) { 
+          // dups get in somehow, and this fixes it
+          if (theatre.data.find((eDf) => { return eDf[1] === df[1] && eDf[2] === df[2] && eDf[3] === df[3] && eDf[4] === df[4]}) == null) {
+            theatre.data.push(df);
+          }
+        }
+      });
     });
 
-    for (var theatreId in data) {
-      data[theatreId].sort((a,b) => parseInt(a[2]) > parseInt(b[2]) ? 1 : -1);
-    }
-    sections.sort((a,b) => this.theatres[a].ordinal > this.theatres[b].ordinal ? 1 : -1);
+    this.data.sort((a, b) => this.theatres[a.key].ordinal > this.theatres[b.key].ordinal ? 1 : -1);
 
-    var dataSource = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1.guid !== r2.guid,
-      sectionHeaderHasChanged: (s1, s2) => s1 !== s2,
-    }).cloneWithRowsAndSections(data, sections);
+    this.data.forEach(theatre => {
+      theatre.data.sort((a,b) => parseInt(a[2]) > parseInt(b[2]) ? 1 : -1);
+    });
 
     var dimensions = Dimensions.get('window');
     var small = (dimensions.width < 350);
     this.state = {
-      dataSource: dataSource,
       small: small,
       imageHeight: small ? 140 : 180
     }
   }
 
   rowPressed(link) {
-    console.log(link);
     if (link != '') {
       Linking.openURL(link);
     }
   }
 
 
-  renderRow(rowData, sectionId, rowId) {
+  renderRow({item}) {
+    const sectionId = item[0];
+    const rowData = item;
     var firstLink = this.listings.theatres[sectionId]['m'][rowData[1]][rowData[2]]['l'];
     var secondLink = this.listings.theatres[sectionId]['m'][rowData[3]][rowData[4]]['l'];
     var timeStyle = (this.state.small ? styles.timeSmall : styles.time);
@@ -120,10 +123,10 @@ class DoubleFeatures extends Component {
     );
   }
 
-  renderSectionHeader(sectionData, sectionID) {
+  renderSectionHeader({section}) {
     return (
       <View style={styles.sectionHeader}>
-          <Text style={styles.sectionHeaderText}>{this.listings.theatres[sectionID]['name']}</Text>
+          <Text style={styles.sectionHeaderText}>{this.listings.theatres[section.key]['name']}</Text>
       </View>
     );
   }
@@ -136,12 +139,13 @@ class DoubleFeatures extends Component {
 
   render() {
     return (
-      <ListView
-        dataSource={this.state.dataSource}
-        renderRow={this.renderRow.bind(this)}
-        renderHeader={this.renderHeader.bind(this)}
+      <SectionList
+        keyExtractor={(item) => {
+          return `${item[0]}-${item[1]}-${item[2]}-${item[3]}-${item[4]}`;
+        }}
+        renderItem={this.renderRow.bind(this)}
         renderSectionHeader={this.renderSectionHeader.bind(this)}
-        renderSeparator={this.renderSeparator.bind(this)}
+        sections={this.data}
       />
     );
   }
