5a108f6159c58ecb8d9b4479b918f8f816ea89a0
Will Carlough <will@carlough.net>
2025-04-24 19:45:02 -0400

    dark mode can suck shit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,3 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
@@ -10,7 +5,6 @@ import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
 import "react-native-reanimated";
 
-import { useColorScheme } from "@/hooks/useColorScheme";
 import { ListingsContext, ManualContext } from "@/constants/Context";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -19,7 +13,6 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const [listings, setListings] = useState(null);
   const [manual, setManual] = useState(false);
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
@@ -35,32 +28,30 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <ManualContext.Provider value={{ manual, setManual }}>
-        <ListingsContext.Provider value={{ listings, setListings }}>
-          <Stack>
-            <Stack.Screen
-              name="index"
-              options={{
-                title: "Search",
-              }}
-            />
-            <Stack.Screen
-              name="SearchResults"
-              options={{
-                title: "Search Results",
-              }}
-            />
-            <Stack.Screen
-              name="DoubleFeatures"
-              options={{
-                title: "Double Features",
-              }}
-            />
-          </Stack>
-          <StatusBar style="auto" />
-        </ListingsContext.Provider>
-      </ManualContext.Provider>
-    </ThemeProvider>
+    <ManualContext.Provider value={{ manual, setManual }}>
+      <ListingsContext.Provider value={{ listings, setListings }}>
+        <Stack>
+          <Stack.Screen
+            name="index"
+            options={{
+              title: "Search",
+            }}
+          />
+          <Stack.Screen
+            name="SearchResults"
+            options={{
+              title: "Search Results",
+            }}
+          />
+          <Stack.Screen
+            name="DoubleFeatures"
+            options={{
+              title: "Double Features",
+            }}
+          />
+        </Stack>
+        <StatusBar style="auto" />
+      </ListingsContext.Provider>
+    </ManualContext.Provider>
   );
 }
