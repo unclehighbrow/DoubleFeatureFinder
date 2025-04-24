00ad4d8fd705b01c104a63e3553994f9595d4aa7
Will Carlough <will@carlough.net>
2025-04-24 16:26:27 -0400

    formatting

--- constants/Util.js
+++ constants/Util.js
@@ -1,34 +1,33 @@
-'use strict';
+"use strict";
 
-import React, { Component } from 'react';
-import {
-  Text,
-  NavigatorIOS,
-  AppRegistry
-} from 'react-native';
+import React, { Component } from "react";
+import { Text, NavigatorIOS, AppRegistry } from "react-native";
 
 class Util extends React.Component {
   static minsToTime(mins) {
     mins = mins % 1440;
-    var meridian = 'AM';
+    var meridian = "AM";
     if (mins >= 720) {
-      meridian = 'PM';
+      meridian = "PM";
       mins -= 720;
     }
-    var minutes = '' + (mins % 60);
+    var minutes = "" + (mins % 60);
     if (minutes.length == 1) {
-      minutes = '0' + minutes;
+      minutes = "0" + minutes;
     }
     var hours = Math.floor(mins / 60);
     if (hours == 0) {
       hours = 12;
     }
-    return '' + hours + ':' + minutes + ' ' + meridian;
+    return "" + hours + ":" + minutes + " " + meridian;
   }
 
   static checkTheatre(theatreId, theatres) {
-    for (var movieId in theatres[theatreId]['m']) {
-      if (Util.findDoubleFeatureMovieIdsInTheatre(theatreId, movieId, theatres).size > 0) {
+    for (var movieId in theatres[theatreId]["m"]) {
+      if (
+        Util.findDoubleFeatureMovieIdsInTheatre(theatreId, movieId, theatres)
+          .size > 0
+      ) {
         return true;
       }
     }
@@ -48,7 +47,11 @@ class Util extends React.Component {
   static findDoubleFeatureMovieIdsInAllTheatres(movieId, theatres) {
     var ret = new Set();
     for (var theatreId in theatres) {
-      var theatreSet = Util.findDoubleFeatureMovieIdsInTheatre(theatreId, movieId, theatres);
+      var theatreSet = Util.findDoubleFeatureMovieIdsInTheatre(
+        theatreId,
+        movieId,
+        theatres
+      );
       for (let movieId of theatreSet) {
         ret.add(movieId);
       }
@@ -58,18 +61,18 @@ class Util extends React.Component {
   static findDoubleFeatures(theatreId, movieId, secondMovieId, theatres) {
     var ret = [];
     var theatre = theatres[theatreId];
-    for (var iMovieId in theatre['m']) {
+    for (var iMovieId in theatre["m"]) {
       if (movieId == 0 || iMovieId == movieId) {
-        for (var showtime in theatre['m'][iMovieId]) {
-          if (theatre['m'][iMovieId][showtime]['b']) {
-            for (let df of theatre['m'][iMovieId][showtime]['b']) {
+        for (var showtime in theatre["m"][iMovieId]) {
+          if (theatre["m"][iMovieId][showtime]["b"]) {
+            for (let df of theatre["m"][iMovieId][showtime]["b"]) {
               if (secondMovieId == 0 || df[0] == secondMovieId) {
                 ret.push([theatreId, df[0], df[1], iMovieId, showtime]);
               }
             }
           }
-          if (theatre['m'][iMovieId][showtime]['a']) {
-            for (let df of theatre['m'][iMovieId][showtime]['a']) {
+          if (theatre["m"][iMovieId][showtime]["a"]) {
+            for (let df of theatre["m"][iMovieId][showtime]["a"]) {
               if (secondMovieId == 0 || df[0] == secondMovieId) {
                 ret.push([theatreId, iMovieId, showtime, df[0], df[1]]);
               }
@@ -82,9 +85,14 @@ class Util extends React.Component {
   }
 
   static findDoubleFeaturesInAllTheatres(movieId, secondMovieId, theatres) {
-    var ret =[];
+    var ret = [];
     for (var theatreId in theatres) {
-      var theatreDffs = Util.findDoubleFeatures(theatreId, movieId, secondMovieId, theatres);
+      var theatreDffs = Util.findDoubleFeatures(
+        theatreId,
+        movieId,
+        secondMovieId,
+        theatres
+      );
       for (let movieId of theatreDffs) {
         ret.push(movieId);
       }
